f8e3ea5f3684360668cff8b89c0f035356eb0e8d
Baran Berkay <48731845+bariskanberkay@users.noreply.github.com>
2024-03-11 01:18:33 +0300

    Update filament-user-activity.php

--- config/filament-user-activity.php
+++ config/filament-user-activity.php
@@ -5,6 +5,7 @@ return [
     'table' => [
         'name' => env('FILAMENT_USER_ACTIVITY_TABLE_NAME', 'user_activities'),
         'retention-days' => env('FILAMENT_USER_ACTIVITY_RETENTION_DAYS', 60),
+        'model' => \App\Models\User\User::class,
         'active-users' => [
             'timeframe-selection' => [
                 15 => '15 Minutes',
