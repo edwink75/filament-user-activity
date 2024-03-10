bbf8a9b072a165f4c12e129652f8b8e5ff123e33
Baran Berkay <48731845+bariskanberkay@users.noreply.github.com>
2024-03-11 01:27:31 +0300

    Update filament-user-activity.php
    Model not changeable

--- config/filament-user-activity.php
+++ config/filament-user-activity.php
@@ -2,7 +2,7 @@
 
 // config for Edwink/FilamentUserActivity
 return [
-    'model' => \App\Models\User\User::class,
+    'model' => \App\Models\User::class,
     'table' => [
         'name' => env('FILAMENT_USER_ACTIVITY_TABLE_NAME', 'user_activities'),
         'retention-days' => env('FILAMENT_USER_ACTIVITY_RETENTION_DAYS', 60),
