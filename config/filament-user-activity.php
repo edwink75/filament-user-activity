09e836de4fc74ca97b8aefedc35c33ba3d4e22db
Baran Berkay <48731845+bariskanberkay@users.noreply.github.com>
2024-03-13 02:31:38 +0300

    Update filament-user-activity.php

--- config/filament-user-activity.php
+++ config/filament-user-activity.php
@@ -10,13 +10,14 @@ return [
         'navigation_icon' => 'heroicon-o-photo',
         'navigation_sort' => null,
         'navigation_count_badge' => false,
+        'should_register_navigation' => true,
         'resource'=>\Edwink\FilamentUserActivity\Filament\Resources\UserActivityResource::class,
     ],
     'is_tenant_aware'=>false,
+    'tenant_ownership_relationship_name'=>"tenant",
     'table' => [
         'name' => env('FILAMENT_USER_ACTIVITY_TABLE_NAME', 'user_activities'),
         'retention-days' => env('FILAMENT_USER_ACTIVITY_RETENTION_DAYS', 60),
-
         'active-users' => [
             'timeframe-selection' => [
                 15 => '15 Minutes',
