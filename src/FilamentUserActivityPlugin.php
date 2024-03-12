e3b5a5f245d19773672bb17736c89289654d0e64
Baran Berkay <48731845+bariskanberkay@users.noreply.github.com>
2024-03-13 02:17:57 +0300

    Update FilamentUserActivityPlugin.php

--- src/FilamentUserActivityPlugin.php
+++ src/FilamentUserActivityPlugin.php
@@ -6,9 +6,29 @@ use Edwink\FilamentUserActivity\Filament\Pages\ActiveUsers;
 use Edwink\FilamentUserActivity\Filament\Resources\UserActivityResource;
 use Filament\Contracts\Plugin;
 use Filament\Panel;
+use Filament\Support\Concerns\EvaluatesClosures;
 
 class FilamentUserActivityPlugin implements Plugin
 {
+
+    use EvaluatesClosures;
+
+    protected string | Closure | null $label = null;
+
+    protected string | Closure | null $navigationGroup = null;
+
+    protected ?string $navigationIcon = null;
+
+    protected ?int $navigationSort = null;
+
+    protected ?bool $navigationCountBadge = null;
+
+    protected ?bool $shouldRegisterNavigation = null;
+
+    protected string | Closure | null $pluralLabel = null;
+
+    protected ?string $resource = null;
+
     public function getId(): string
     {
         return 'filament-user-activity';
@@ -18,11 +38,108 @@ class FilamentUserActivityPlugin implements Plugin
     {
         $panel
             ->resources([
-                UserActivityResource::class,
+                $this->getResource(),
             ])
             ->pages([
                 ActiveUsers::class,
             ]);
+    }
+
+    public function getResource(): string
+    {
+
+        return $this->resource ?? config('filament-user-activity.resources.resource');
+    }
+
+    public function getLabel(): string
+    {
+        return $this->evaluate($this->label) ?? config('filament-user-activity.resources.label');
+    }
+
+    public function getPluralLabel(): string
+    {
+        return $this->evaluate($this->pluralLabel) ?? config('filament-user-activity.resources.plural_label');
+    }
+
+    public function getNavigationGroup(): ?string
+    {
+        return $this->evaluate($this->navigationGroup) ?? config('filament-user-activity.resources.navigation_group');
+    }
+
+    public function getNavigationIcon(): ?string
+    {
+        return $this->navigationIcon ?? config('filament-user-activity.resources.navigation_icon');
+    }
+
+    public function getNavigationSort(): ?int
+    {
+        return $this->navigationSort ?? config('filament-user-activity.resources.navigation_sort');
+    }
+
+    public function getNavigationCountBadge(): ?bool
+    {
+        return $this->navigationCountBadge ?? config('filament-user-activity.resources.navigation_count_badge');
+    }
+
+    public function shouldRegisterNavigation(): ?bool
+    {
+        return $this->shouldRegisterNavigation ?? config('filament-user-activity.should_register_navigation');
+    }
+
+    public function navigationGroup(string | Closure | null $group = null): static
+    {
+        $this->navigationGroup = $group;
+
+        return $this;
+    }
+
+    public function navigationIcon(string $icon): static
+    {
+        $this->navigationIcon = $icon;
+
+        return $this;
+    }
+
+    public function navigationSort(int $order): static
+    {
+        $this->navigationSort = $order;
+
+        return $this;
+    }
+
+    public function navigationCountBadge(bool $show = true): static
+    {
+        $this->navigationCountBadge = $show;
+
+        return $this;
+    }
+
+    public function registerNavigation(bool $show = true): static
+    {
+        $this->shouldRegisterNavigation = $show;
+
+        return $this;
+    }
+
+    public function pluralLabel(string | Closure $label): static
+    {
+        $this->pluralLabel = $label;
+
+        return $this;
+    }
+
+    public function resource(string $resource): static
+    {
+        $this->resource = $resource;
+
+        return $this;
+    }
+
+    public function label(string | Closure $label): static
+    {
+        $this->label = $label;
+
+        return $this;
     }
 
     public function boot(Panel $panel): void
