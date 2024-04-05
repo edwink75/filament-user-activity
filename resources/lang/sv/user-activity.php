aef3bbff4e0e88879916a5b88c52f1f10351bcf1
Jimmie Johansson <jimmitjoo@gmail.com>
2024-04-05 13:36:21 +0200

    Create user-activity.php
    Add swedish translations

---
+++ resources/lang/sv/user-activity.php
@@ -0,0 +1,24 @@
+<?php
+
+// translations for Edwink/FilamentUserActivity
+return [
+    'currently-active-users' => 'Aktiva användare',
+    'resource' => [
+        'navigation' => 'Administration',
+        'model_label' => 'Användaraktivitet',
+        'model_plural_label' => 'Användaraktiviteter',
+    ],
+    'active-users-table' => [
+        'column' => [
+            'last_action_count' => 'Antal aktiviteter',
+            'last_action_time' => 'Senaste aktivitet',
+        ],
+    ],
+    'activity-table' => [
+        'column' => [
+            'time' => 'Datum och tid',
+            'url' => 'Url',
+            'user' => 'Användare',
+        ],
+    ],
+];
